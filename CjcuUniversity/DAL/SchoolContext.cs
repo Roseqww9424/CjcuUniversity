8e24a1885b6baf3657645eaeb7722397e8ad3586
Roseqww9424 <qha22ppy@gmail.com>
2019-04-24 15:19:30 +0800

    first commit

---
+++ CjcuUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using CjcuUniversity.Models;
+
+namespace CjcuUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        /* 指定的連接字串 */
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        /* 防止資料表名稱的複數化 */
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
